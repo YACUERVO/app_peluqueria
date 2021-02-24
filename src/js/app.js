c98305d19bdbe9a8d6140ad1c4e163e050039e6a
YAMITH ARLEY CUERVO CORREDOR <yamithcuervo@gmail.com>
2021-02-24 14:45:16 -0500

    ejercicios gulp 2

---
+++ src/js/app.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+
+});
+
+function iniciarApp() {
+    mostrarServicios();
+}
+//try catch sirve si una concexion se pudo iniciar a una base de datos
+async function mostrarServicios() { //funcion para validar la base de datos. En caso que no halla resultados sigue funcionando 
+    try { //estamos llamando toda la hoja de servicios de servici.jason para llevarlo al HTML
+        const resultado = await fetch('./servicios.json'); //fetch para llamar la base de datos de jason
+        const db = await resultado.json(); //para saber el tipo de datos al estilo objetos en javas crip
+        const { servicios } = db;
+
+        //GENERAR EL HTML
+
+        servicios.forEach(servicio => {
+            const { id, nombre, precio } = servicio;
+
+            //DOM SCRIPTING
+            //generar el nombre
+            const nombreServicio = document.createElement('P')
+            nombreServicio.textContent = nombre
+            nombreServicio.classList.add('nombre__servicio')
+
+
+            //generar el precio del servicio
+            const precioServicio = document.createElement('P')
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio__servicio')
+
+            //generar div contenedor de servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+
+            //inyectar precio y nombre al div de servicio
+
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+            //El método appendChild() inserta un nuevo nodo dentro de la estructura DOM de un documento, y es la segunda parte del proceso central uno-dos, crear-y-añadir para construir páginas web a base de programación
+
+            //intectarlo en el HTML
+            document.querySelector('#servicios').appendChild(servicioDiv)
+
+        });
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
